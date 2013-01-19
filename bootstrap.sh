c27c13857b23dafd1efbedae69ab9f37b5ff1f46
Mathias Fussenegger <f.mathias@zignar.net>
2013-01-19 01:08:03 +0100

    add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sed -i "s/SITENAME/${1}/g" setup.py SITENAME/*.py SITENAME/models/__init__.py SITENAME/handlers/__init__.py
+
+if [[ -d "SITENAME" ]]; then
+    mv SITENAME $1
+fi
+
+if [[ -a "README.md" ]]; then
+    echo -e "# $1\n\nPlease write a useful readme." > README.md
+fi
+
+if [[ -x "/usr/bin/virtualenvwrapper.sh" ]]; then
+    source /usr/bin/virtualenvwrapper.sh
+    mkvirtualenv -p python3.3 $1
+    pip install -r requirements.txt
+else
+    echo "No virtualenvwrapper.sh found. No virtualenv created!"
+fi
+
+rm -rf .git
+git init .
